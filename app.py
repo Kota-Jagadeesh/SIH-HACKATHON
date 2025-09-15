96f41e7d2eb4b51246f50df3205021859bd06613
naveen28204280 <naveensrinivas282@gmail.com>
2025-09-15 14:12:48 +0530

    feat: login function

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session 
 import os
 import bcrypt
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
 db = SQLAlchemy(app)
 CORS(app)
+Session(app)
 
-class user(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, auto_increment=True)
     name = db.Column(db.String(255))
     username = db.Column(db.String(255), unique=True)
@@ -24,7 +27,21 @@ def register():
     password_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt(rounds=12)
     hash = bcrypt.hashpw(password_bytes, salt)
-    user = user(name=data.name, username=data.username, password=hash)
+    user = User(name=data.name, username=data.username, password=hash)
     db.add(user)
     db.commit()
     return jsonify({"Success": "Registered user"}), 200
+
+@app.route('/api/login/', methods=['POST'])
+def login():
+    data = request.json()
+    if not all(i in data for i in ['username', 'password']):
+        return jsonify({'error': 'Missing required fields'}), 200
+    user = db.query(User).fitler(username=data.username)
+    if not user:
+        return jsonify({'error': 'No such user found'}), 404
+    if bcrypt.checkpw(user.password):
+        session['username'] = user.username
+        return jsonify({'success': 'logged in'}), 200
+    else:
+        return jsonify({'fail': 'Incorrect password'}), 403
