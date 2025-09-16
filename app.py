94fdf40be37ee92c19a26e4ffc1b79e21a53f829
naveen28204280 <naveensrinivas282@gmail.com>
2025-09-16 21:09:36 +0530

    feat: logout function

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, jsonify, request, session
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from flask_session import Session 
+from flask_session import Session
 import os
 import bcrypt
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv('SECRET_KEY')
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URI")
 db = SQLAlchemy(app)
 CORS(app)
 Session(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, auto_increment=True)
@@ -18,13 +19,14 @@ class User(db.Model):
     username = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(511), password=True)
 
-@app.route('/api/register/', methods=["POST"])
+
+@app.route("/api/register/", methods=["POST"])
 def register():
     data = request.json()
-    if not all(i in data for i in ['name', 'username', 'password']):
-        return jsonify({'error': 'Missing required fields'}), 400
+    if not all(i in data for i in ["name", "username", "password"]):
+        return jsonify({"error": "Missing required fields"}), 400
     password = data.password
-    password_bytes = password.encode('utf-8')
+    password_bytes = password.encode("utf-8")
     salt = bcrypt.gensalt(rounds=12)
     hash = bcrypt.hashpw(password_bytes, salt)
     user = User(name=data.name, username=data.username, password=hash)
@@ -32,22 +34,24 @@ def register():
     db.commit()
     return jsonify({"Success": "Registered user"}), 200
 
-@app.route('/api/login/', methods=['POST'])
+
+@app.route("/api/login/", methods=["POST"])
 def login():
     data = request.json()
-    if not all(i in data for i in ['username', 'password']):
-        return jsonify({'error': 'Missing required fields'}), 200
+    if not all(i in data for i in ["username", "password"]):
+        return jsonify({"error": "Missing required fields"}), 200
     user = db.query(User).fitler(username=data.username)
     if not user:
-        return jsonify({'error': 'No such user found'}), 404
+        return jsonify({"error": "No such user found"}), 404
     if bcrypt.checkpw(user.password):
-        session['username'] = user.username
-        return jsonify({'success': 'logged in'}), 200
+        session["username"] = user.username
+        return jsonify({"success": "logged in"}), 200
     else:
-        return jsonify({'fail': 'Incorrect password'}), 403
+        return jsonify({"fail": "Incorrect password"}), 403
 
-@app.route('/api/logout', methods=['POST'])
+
+@app.route("/api/logout", methods=["POST"])
 def logout():
     data = request.json()
     session.pop()
-    return jsonify({'success': 'user logged out'}), 200
+    return jsonify({"success": "user logged out"}), 200
